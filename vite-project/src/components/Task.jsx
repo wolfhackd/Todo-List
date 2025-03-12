0e1b255198539b56b5fede4c0efe22de7c7b5be3
Mauro Leal <mauro.costa.12.j@hotmail.com>
2025-03-11 23:55:57 -0300

    Mudança

--- vite-project/src/components/Task.jsx
+++ vite-project/src/components/Task.jsx
@@ -7,22 +7,11 @@ const Task = () => {
   return (
     <>
     <div className='task'>
-        <h2 className='task__title'>TaskName</h2>
-        <ul>
-            {/* Arrumar os ids */}
-            <li className="task__item">
-                <input type="checkbox" id='task__checkbox' name='task__checkbox'/>
-                <label htmlFor="task__checkbox">Comprar Pão</label>
-            </li>
-            <li className="task__item">
-                <input type="checkbox" id='task__checkbox' name='task__checkbox'/>
-                <label htmlFor="task__checkbox">Comprar Pão 2</label>
-            </li>
-            <li className="task__item">
-                <input type="checkbox" id='task__checkbox' name='task__checkbox'/>
-                <label htmlFor="task__checkbox">Comprar Pão 3</label>
-            </li>
-        </ul>
+        <h2 className='task__title'>Taskname</h2>
+        <p>11/03/2025</p>
+        <p>11/04/2025</p>
+        <p>Concluido</p>
+        <span>=</span>
     </div>
     </>
   )
