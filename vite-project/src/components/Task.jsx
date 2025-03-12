7564f1f7fca03fce592e4644ed3529c5580ed0c7
Mauro Leal <mauro.costa.12.j@hotmail.com>
2025-03-11 23:43:11 -0300

    grid Completo

--- vite-project/src/components/Task.jsx
+++ vite-project/src/components/Task.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 
+// Posso criar um componente para as tasks list
+// Posso criar um componente para o controlador
+
 const Task = () => {
   return (
     <>
     <div className='task'>
         <h2 className='task__title'>TaskName</h2>
         <ul>
+            {/* Arrumar os ids */}
             <li className="task__item">
-                <input type="checkbox" className='task__checkbox' name='task__checkbox'/>
+                <input type="checkbox" id='task__checkbox' name='task__checkbox'/>
                 <label htmlFor="task__checkbox">Comprar Pão</label>
             </li>
             <li className="task__item">
-                <input type="checkbox" className='task__checkbox' name='task__checkbox'/>
+                <input type="checkbox" id='task__checkbox' name='task__checkbox'/>
                 <label htmlFor="task__checkbox">Comprar Pão 2</label>
             </li>
             <li className="task__item">
-                <input type="checkbox" className='task__checkbox' name='task__checkbox'/>
+                <input type="checkbox" id='task__checkbox' name='task__checkbox'/>
                 <label htmlFor="task__checkbox">Comprar Pão 3</label>
             </li>
         </ul>
--- vite-project/src/components/TodoHub.jsx
+++ vite-project/src/components/TodoHub.jsx
@@ -11,10 +11,6 @@ const TodoHub = () => {
       <Task />
       <Task />
       <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
     </div>
     </>
   )
